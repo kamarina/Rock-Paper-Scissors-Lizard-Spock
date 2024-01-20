09079a4473f3ea8512d180dcf2db60ac457bc4ce
Maryna Karlovska <65891455+kamarina@users.noreply.github.com>
2024-01-20 16:53:49 +0000

    Update main.py
    added documentation, adjusted the code to meet PEP 8

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ WIN = 0
 
 
 def game_menu():
-    """Display the game menu."""
-    print("-" * 50)
-    print("Welcome to Rock-Paper-Scissors-Lizard-Spock Game!")
-    print("In this game, you can choose from the following options:")
+    """Display the game menu"""
+    print(f"{"-" * 50}\n "
+          f"Welcome to Rock-Paper-Scissors-Lizard-Spock Game!\n"
+          f"In this game, you can choose from the following options:")
     for i in range(len(GAME_LIST)):
         print(f"'{GAME_LIST[i]}' for {ITEMS_LIST[i]} - {SYMBOLS[i]}")
 
@@ -32,6 +32,7 @@ def user_choice(player_name):
 
 
 def computer_choice():
+    """Get computer's choice"""
     print("Computer is choosing... ")
     for _ in range(2):
         for symbol in SYMBOLS:
@@ -47,6 +48,7 @@ def computer_choice():
 
 
 def continue_playing():
+    """Ask the user if they want to play again"""
     while True:
         user_answer = input("Do you want to play again? (y/n)").lower()
         if user_answer == "y":
@@ -57,7 +59,8 @@ def continue_playing():
             print("Please enter 'y' or 'n'.")
 
 
-def game():
+def play_game():
+    """Execute the main game loop based on the user's choice"""
     while True:
         print("Welcome to Rock-Paper-Scissors-Lizard-Spock Game!")
         print("Select mode:")
@@ -88,17 +91,14 @@ def game():
             index_user = GAME_LIST.index(user_result)
             index_opponent = GAME_LIST.index(user_result_2)
 
-            print("\033[34m-\033[0m" * 40)
-            print(
-                f"\n{player_name}: {ITEMS_LIST[index_user]} - {SYMBOLS[index_user]}  🆚  {SYMBOLS[index_opponent]} - {ITEMS_LIST[index_opponent]} :{opponent_name}")
-            print("\033[34m-\033[0m" * 40)
-
-            print(f"\033[33mRESULT: ", end="")
-
+            print(f"{'\033[34m-\033[0m' * 40}\n{player_name}: {ITEMS_LIST[index_user]} "
+                  f"- {SYMBOLS[index_user]}  🆚  {SYMBOLS[index_opponent]} - {ITEMS_LIST[index_opponent]} :"
+                  f"{opponent_name}\n{'\033[34m-\033[0m' * 40} \n\033[33mRESULT: ", end="")
             if user_result == user_result_2:
-                print("It's a tie!\033[0m")
-                print(f"\n\033[32m{player_name} Win: {user_win} \n{opponent_name} Win: {comp_win}\033[0m")
-                print("\033[34m-\033[0m" * 40)
+                print(
+                    f"\033[0mIt's a tie!\n\033[32m{player_name} Win: {user_win} \n{opponent_name} "
+                    f"Win: {comp_win}\n{'\033[34m-\033[0m' * 40}")
+
             elif (
                     (user_result == 'r' and (user_result_2 == 's' or user_result_2 == 'l')) or
                     (user_result == 's' and (user_result_2 == 'p' or user_result_2 == 'l')) or
@@ -106,18 +106,19 @@ def game():
                     (user_result == 'l' and (user_result_2 == 'k' or user_result_2 == 'p')) or
                     (user_result == 'k' and (user_result_2 == 's' or user_result_2 == 'r'))
             ):
-                print(f"{player_name} wins!!! ✨\033[0m")
-                user_win += 1
-                print(f"\n\033[32m{player_name} Win: {user_win} \n{opponent_name} Win: {comp_win}\033[0m")
-                print("\033[34m-\033[0m" * 40)
+                print(f"{player_name} wins!!! ✨\033[0m\n\033[32m{player_name} Win: {user_win + 1} "
+                      f"\n{opponent_name} Win: {comp_win}\n{'\033[34m-\033[0m' * 40}")
+
             else:
                 print(f"{opponent_name} wins!!! 😑\033[0m")
+
                 comp_win += 1
-                print(f"\n\033[32m{player_name} Win: {user_win} \n{opponent_name} Win: {comp_win}\033[0m")
-                print("\033[34m-\033[0m" * 40)
+                print(f"\n\033[32m{player_name} Win: {user_win} \n{opponent_name} "
+                      f"Win: {comp_win + 1}\033[0m\n{'\033[34m-\033[0m' * 40}")
 
             if not continue_playing():
                 print("Goodbye 👋")
                 break
 
-game()
+
+play_game()
